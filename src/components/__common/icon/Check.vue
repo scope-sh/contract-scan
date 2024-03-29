eddc80a7bb4e1039fd813676efee34c3fe329eb7
Timur Badretdinov <accounts@destiner.io>
2024-03-29 06:33:06 +0000

    Add Check icon

---
+++ src/components/__common/icon/Check.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    viewBox="0 0 15 15"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill="currentColor"
+      fill-rule="evenodd"
+      d="M11.467 3.727c.289.189.37.576.181.865l-4.25 6.5a.625.625 0 0 1-.944.12l-2.75-2.5a.625.625 0 0 1 .841-.925l2.208 2.007l3.849-5.886a.625.625 0 0 1 .865-.181"
+      clip-rule="evenodd"
+    />
+  </svg>
+</template>
