27a4bad0af9631fdffe38f8841df6af3bc5c9d8f
Timur Badretdinov <accounts@destiner.io>
2024-03-29 06:33:33 +0000

    Add ButtonCopy component

---
+++ src/components/contract/ButtonCopy.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    class="button"
+    @click="copy"
+  >
+    <IconCopy
+      v-if="ready"
+      class="icon"
+    />
+    <IconCheck
+      v-else
+      class="icon"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useTimeout } from '@vueuse/core';
+import { onMounted } from 'vue';
+
+import IconCheck from '@/components/__common/icon/Check.vue';
+import IconCopy from '@/components/__common/icon/Copy.vue';
+
+const props = defineProps<{
+  value: string;
+}>();
+
+onMounted(() => {
+  stop();
+});
+
+const { ready, start, stop } = useTimeout(2000, { controls: true });
+
+function copy(): void {
+  navigator.clipboard.writeText(props.value);
+  start();
+}
+</script>
+
+<style scoped>
+.button {
+  padding: 4px;
+  border: 1px solid var(--color-border-tertiary);
+  border-radius: var(--border-radius-m);
+  cursor: pointer;
+}
+
+.button:hover {
+  border: 1px solid var(--color-border-quaternary);
+}
+
+.icon {
+  width: 16px;
+  height: 16px;
+}
+</style>
