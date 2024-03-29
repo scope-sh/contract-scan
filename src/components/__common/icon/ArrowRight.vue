2677c9ec37e044288b9907a450c878bdf8e119ba
Timur Badretdinov <accounts@destiner.io>
2024-03-29 05:15:04 +0000

    Add ArrowRight component

---
+++ src/components/__common/icon/ArrowRight.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    viewBox="0 0 15 15"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill="currentColor"
+      fill-rule="evenodd"
+      d="M8.146 3.146a.5.5 0 0 1 .708 0l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L11.293 8H2.5a.5.5 0 0 1 0-1h8.793L8.146 3.854a.5.5 0 0 1 0-.708"
+      clip-rule="evenodd"
+    />
+  </svg>
+</template>
