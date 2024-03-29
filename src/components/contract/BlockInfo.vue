15bd2c1bb64a3b60dc1ae5e1181d76461edd556a
Timur Badretdinov <accounts@destiner.io>
2024-03-29 06:33:27 +0000

    Add BlockInfo component

---
+++ src/components/contract/BlockInfo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="block">
+    <BlockStatus :status="'success'">exact bytecode match</BlockStatus>
+    <BlockStatus :status="'warning'">different bytecode</BlockStatus>
+    <BlockStatus :status="'empty'">not deployed (no code)</BlockStatus>
+    <BlockStatus :status="'progress'">querying</BlockStatus>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BlockStatus from './BlockStatus.vue';
+</script>
+
+<style scoped>
+.block {
+  display: flex;
+  gap: 20px;
+  padding: 12px 9px;
+  border: 1px solid var(--color-border-primary);
+  border-radius: var(--border-radius-medium);
+  background: var(--color-background-secondary);
+}
+</style>
