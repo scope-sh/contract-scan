9cd545d96361ea85028cf61f5d9401b6a8fb59a3
Timur Badretdinov <accounts@destiner.io>
2024-03-29 06:33:21 +0000

    Add BlockStatus component

---
+++ src/components/contract/BlockStatus.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="status"
+    :class="{
+      success: status === 'success',
+      warning: status === 'warning',
+      empty: status === 'empty',
+      progress: status === 'progress',
+    }"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps<{ status: Status }>();
+</script>
+
+<script lang="ts">
+type Status = 'success' | 'warning' | 'empty' | 'progress';
+
+export type { Status };
+</script>
+
+<style scoped>
+.success {
+  color: #4dbf8c;
+}
+
+.warning {
+  color: #c8b866;
+}
+
+.empty {
+  color: #4f4f4f;
+}
+
+.progress {
+  color: #818181;
+}
+</style>
