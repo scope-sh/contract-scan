c94d1cd38254927a952ef28e11f241c7f1fff501
Timur Badretdinov <accounts@destiner.io>
2024-04-23 18:31:37 +0100

    Import react in OG API

--- api/og.tsx
+++ api/og.tsx
@@ -1,35 +1,30 @@
-import { ImageResponse } from '@vercel/og';
 import type { VercelRequest } from '@vercel/node';
- 
+import { ImageResponse } from '@vercel/og';
+import React from 'react';
+
 export const config = {
   runtime: 'edge',
 };
-export default function handler(request: VercelRequest) {
+export default function handler(request: VercelRequest): Response {
   try {
-    if (!request.url)  {
+    if (!request.url) {
       return new Response(`Failed to generate the image`, {
         status: 400,
       });
     }
     const { searchParams } = new URL(request.url);
- 
+
     // ?title=<title>
     const hasTitle = searchParams.has('title');
     const title = hasTitle
       ? searchParams.get('title')?.slice(0, 100)
       : 'My default title';
- 
-    return new ImageResponse(
-      (
-        <div>{ title }</div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-      },
-    );
-  } catch (e: any) {
-    console.log(`${e.message}`);
+
+    return new ImageResponse(<div>{title}</div>, {
+      width: 1200,
+      height: 630,
+    });
+  } catch (e) {
     return new Response(`Failed to generate the image`, {
       status: 500,
     });
