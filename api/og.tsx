07c7b5d8aed9bf23aa68d44c7ed558c4fddc1140
Timur Badretdinov <accounts@destiner.io>
2024-04-23 19:32:08 +0100

    Update OG API endpoint

--- api/og.tsx
+++ api/og.tsx
@@ -1,6 +1,7 @@
 import type { VercelRequest } from '@vercel/node';
 import { ImageResponse } from '@vercel/og';
 import React from 'react';
+import { isAddress } from 'viem';
 
 export const config = {
   runtime: 'edge',
@@ -12,18 +13,122 @@ export default function handler(request: VercelRequest): Response {
         status: 400,
       });
     }
-    const { searchParams } = new URL(request.url);
+    const { pathname } = new URL(request.url);
+    const tokens = pathname.split('/').at(-1);
+    const address = tokens && isAddress(tokens) ? tokens : null;
 
-    // ?title=<title>
-    const hasTitle = searchParams.has('title');
-    const title = hasTitle
-      ? searchParams.get('title')?.slice(0, 100)
-      : 'My default title';
+    if (!address) {
+      return new ImageResponse(
+        (
+          <div
+            style={{
+              display: 'flex',
+              height: '100%',
+              width: '100%',
+              alignItems: 'center',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              gap: 44,
+              backgroundImage:
+                'linear-gradient(to bottom right, #0A0A0C, #17171E)',
+              letterSpacing: -1,
+              fontWeight: 400,
+              textAlign: 'center',
+            }}
+          >
+            <div
+              style={{
+                color: '#EDEDED',
+                fontSize: 48,
+              }}
+            >
+              Contract Scan
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                color: '#A1A1A1',
+                fontSize: 32,
+                gap: 4,
+              }}
+            >
+              <div>Search deployed contracts</div>
+              <div>across 50+ EVM chains</div>
+            </div>
+          </div>
+        ),
+        {
+          width: 1200,
+          height: 630,
+        },
+      );
+    }
 
-    return new ImageResponse(<div>{title}</div>, {
-      width: 1200,
-      height: 630,
-    });
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            display: 'flex',
+            height: '100%',
+            width: '100%',
+            alignItems: 'flex-start',
+            justifyContent: 'space-between',
+            flexDirection: 'column',
+            gap: 44,
+            backgroundImage:
+              'linear-gradient(to bottom right, #0A0A0C, #17171E)',
+            letterSpacing: -1,
+            fontWeight: 400,
+            textAlign: 'center',
+            padding: 32,
+          }}
+        >
+          <div
+            style={{
+              color: '#A1A1A1',
+              fontSize: 24,
+            }}
+          >
+            Contract Scan
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 32,
+            }}
+          >
+            <div
+              style={{
+                color: '#EDEDED',
+                fontSize: 48,
+                gap: 32,
+              }}
+            >
+              Contract
+            </div>
+            <div
+              style={{
+                color: '#7B7B7B',
+                background: '#1B1B1B',
+                borderRadius: 8,
+                fontSize: 36,
+                padding: '4px 8px',
+                fontFamily: 'monospace',
+              }}
+            >
+              {address}
+            </div>
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      },
+    );
   } catch (e) {
     return new Response(`Failed to generate the image`, {
       status: 500,
